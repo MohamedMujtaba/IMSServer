24d19a8adc115fd7620102506a3305103e9241da
MohamedMujtaba <65574561+MohamedMujtaba@users.noreply.github.com>
2021-05-30 20:42:35 +0200

    cors-3

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const cors = require("cors");
+// const cors = require("cors");
 const data = require("./allData/semesterOne");
 const allowedOrigins = ["http://localhost:3000", "http://localhost:8000"];
 
 app.use("/stats", express.static("./stats/"));
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does  " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
